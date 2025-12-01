2e3a9a069160ddc738db75b76ac5f93412b285b5
chavimiller <chaviweisman@gmail.com>
2025-12-01 10:08:09 +0200

    Create and outline controllers needed

---
+++ controllers/authController.js
@@ -0,0 +1,19 @@
+// const db = require("../db/queries.js")
+
+// function for GET sign up form
+
+// POST sign up form
+
+// GET login form
+
+// Post login form
+
+// GET member form
+
+// POST member form
+
+// GET admin form
+
+// POST admin form
+
+module.exports = {};
---
+++ controllers/messagesController.js
@@ -0,0 +1,11 @@
+// const db = require("../db/queries.js")
+
+// GET new message form
+
+// POST new message
+
+// POST delete message (admins only)
+
+// GET logout
+
+module.exports = {};
