2f382a3c4596fd6ae9e8e4ebd611f5aeccc1a8f4
chavimiller <chaviweisman@gmail.com>
2025-12-01 10:35:55 +0200

    Outline queries need to call on db

---
+++ db/queries.js
@@ -0,0 +1,27 @@
+const pool = require("./pool");
+
+// messageController.js :
+
+// getMessageById() {}
+
+// insertMessage() {}
+
+// deleteMessage() {}
+
+// indexController.js :
+
+// getAllMessages() {}
+
+// authController.js :
+
+// insertUser() {}
+
+// getUserById(userId) {}
+
+// getUserByUsername(username) {}
+
+// updateMembershipStatus(userId) {}
+
+// updateAdminStatus(userId) {}
+
+module.exports = {};
