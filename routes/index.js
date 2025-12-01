1e924781c91a6c367250320f2a1fc029ba5d9996
chavimiller <chaviweisman@gmail.com>
2025-12-01 11:19:20 +0200

    Outline route files

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
+const indexController = require("../controllers/indexController");
 
 const indexRouter = Router();
+
+// get route for showing all messages
 
 module.exports = indexRouter;
--- routes/messages.js
+++ routes/messages.js
@@ -1,5 +1,13 @@
 const { Router } = require("express");
-
+const messageController = require("../controllers/messagesController");
 const messagesRouter = Router();
+
+// get new message form
+
+// post new message
+
+// post delete a message
+
+// get logout
 
 module.exports = messagesRouter;
