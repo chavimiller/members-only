e0efaa87bf87ff5acf8c5ae918873fd97842a89b
chavimiller <chaviweisman@gmail.com>
2025-12-02 12:37:17 +0200

    Add logic for passport localstrategy, add in needed queries for user lookup by id and username

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,37 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const db = require("../db/queries");
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await db.getUserByUsername(username);
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      // replace with bcrypt
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.getUserById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+module.exports = passport;
--- db/queries.js
+++ db/queries.js
@@ -26,12 +26,20 @@ async function insertUser(firstName, lastName, username, password) {
   );
 }
 
-// getUserById(userId) {}
+async function getUserById(id) {
+  const { rows } = await pool.query(`SELECT * FROM users WHERE id = $1`, [id]);
+  return rows[0];
+}
 
-// getUserByUsername(username) {}
+async function getUserByUsername(username) {
+  const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [
+    username,
+  ]);
+  return rows[0];
+}
 
 // updateMembershipStatus(userId) {}
 
 // updateAdminStatus(userId) {}
 
-module.exports = { insertUser, insertMessage };
+module.exports = { insertUser, insertMessage, getUserById, getUserByUsername };
