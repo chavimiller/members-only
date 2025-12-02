9a17cd845f1401eb2bf9c9065b259ccca41a53ba
chavimiller <chaviweisman@gmail.com>
2025-12-02 10:10:09 +0200

    Create separate validators folder, move validation logic

---
+++ validators/messageValidator.js
@@ -0,0 +1,18 @@
+const { body } = require("express-validator");
+
+const validateMessage = [
+  body("title")
+    .trim()
+    .notEmpty()
+    .withMessage("Title is required.")
+    .isLength({ min: 1, max: 60 })
+    .withMessage("Title must be between 1 and 60 characters."),
+  body("message")
+    .trim()
+    .notEmpty()
+    .withMessage("Message is required.")
+    .isLength({ min: 1, max: 500 })
+    .withMessage("Message must be between 1 and 500 characters"),
+];
+
+module.exports = validateMessage;
