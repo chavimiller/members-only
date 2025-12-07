df638ddd6bc341d440f19e295379b7a361774574
chavimiller <chaviweisman@gmail.com>
2025-12-07 14:13:02 +0200

    Change pool config to connect to Neon cloud db

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,6 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
